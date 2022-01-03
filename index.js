cc02d3c2a5e6dfb83a5c507b4307eba6d2085f46
Пузыно Эдуард <draconus72@gmail.com>
2022-01-03 17:29:40 +0300

    feature/DB-3: ?

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+require("dotenv").config();
 const app = require("./delivery/index.js");
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 5000;
+const sequelize = require("./db.js");
 
 const main = async () => {
-  await app.listen(port, () => {
-    console.log(`This app listening at http://localhost:${port}`);
-  });
+  
+  try {
+    await sequelize.authenticate;
+    await sequelize.sync;
+    await app.listen(PORT, () => {
+      console.log(`This app listening at http://localhost:${PORT}`);
+    });
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 };
 
 main();
